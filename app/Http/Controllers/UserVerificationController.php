3f0bdf533342a469e2f713978b4393ff8bc8eb93
Christian Eaton <chris.eaton@microangelo.co.uk>
2022-09-22 11:46:27 +0100

    Adding User role to email users upon verification of email address [MP-71]

--- app/Http/Controllers/UserVerificationController.php
+++ app/Http/Controllers/UserVerificationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Role;
 use Carbon\Carbon;
 
 class UserVerificationController extends Controller
@@ -17,6 +18,9 @@ class UserVerificationController extends Controller
         $user->verified_at        = Carbon::now()->format(config('panel.date_format') . ' ' . config('panel.time_format'));
         $user->verification_token = null;
         $user->save();
+
+        // make them a User if they're not already
+        $user->roles()->syncWithoutDetaching(2);
 
         return redirect()->route('login')->with('message', trans('global.emailVerificationSuccess'));
     }
