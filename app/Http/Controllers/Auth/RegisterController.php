9d2e2a8761d5af28697ad8b754b3ed001e2c639b
Christian Eaton <chris.eaton@microangelo.co.uk>
2022-12-16 14:38:56 +0000

    Assigning 'User' Role when adding newly registed Users [MP-19, 71]

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -60,10 +60,15 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name'     => $data['name'],
             'email'    => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+        // Assign the user a "User" role
+        $user->roles()->syncWithoutDetaching(2);
+
+        return $user;
     }
 }
